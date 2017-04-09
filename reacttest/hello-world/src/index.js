04e7e07253bf92c59e6625bfa09b88ae522bafab
freebreaker <592378845@qq.com>
2017-04-09 23:29:52 +0800

    添加welcomejs

--- reacttest/hello-world/src/index.js
+++ reacttest/hello-world/src/index.js
@@ -3,7 +3,29 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+// ReactDOM.render(
+//   <h1>Hello, world!</h1>,
+//   document.getElementById('root2')
+// );
+
+// function tick() {
+//   const element = (
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//     </div>
+//   );
+//   ReactDOM.render(
+//     element,
+//     document.getElementById('root2')
+//   );
+// }
+
+// setInterval(tick, 1000);
+//其他 import 略
+import Welcome from './Welcome'
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('root2')
-);
+  <Welcome/>,
+  document.getElementById('root')
+)
