5d9c9f9ce15b6ab6a6d0b5d4f0c230b7c792b352
freebreaker <592378845@qq.com>
2017-04-19 16:19:25 +0800

    将代码移到 componentDidUpdate

--- reacttodo/todo/src/App.js
+++ reacttodo/todo/src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import TodoInput from './TodoInput.js';
-import TodoItem from './TodoItem.js';
+import TodoInput from './TodoInput';
+import TodoItem from './TodoItem';
 import 'normalize.css';
 import "./reset.css";
 import "./App.css";
 import "./TodoItem.css";
 import "./TodoInput.css";
-import Sortable from 'sortablejs'
+import Sortable from 'sortablejs';
+import * as localStore from './localStore'
 
 
 class App extends Component {
@@ -29,10 +30,11 @@ class App extends Component {
     super(props)
     this.state={
       newTodo:'',
-      todoList:[
-        // {id:1,title:'第一个待办'},
-        // {id:2, title:'第二个待办'}
-      ]
+      // todoList:[
+      //   // {id:1,title:'第一个待办'},
+      //   // {id:2, title:'第二个待办'}
+      // ]
+      todoList: localStore.load('todoList') || []
     }
   }
 
@@ -71,17 +73,22 @@ class App extends Component {
               animation: 100
          });
       }
+  componentDidUpdate(){
+    localStore.save('todoList',this.state.todoList)
+  }
 
 
 
        delete(event, todo){
         todo.deleted = true
         this.setState(this.state) 
+        // localStorage.save('todoList',this.state.todoList)
     }
 
       toggle(e, todo){
         todo.status = todo.status === 'completed' ? '' : 'completed'
-        this.setState(this.state) 
+        this.setState(this.state)
+        //localStorage.save('todoList',this.state.todoList) 
     }
 
 
@@ -91,6 +98,7 @@ class App extends Component {
         newTodo: event.target.value,
         todoList: this.state.todoList
       })
+      //localStorage.save('todoList',this.state.todoList)
     }
 
 
@@ -109,6 +117,7 @@ class App extends Component {
         newTodo: '',
         todoList: this.state.todoList
       })
+     // localStorage.save('todoList',this.state.todoList)
   }
 
 
